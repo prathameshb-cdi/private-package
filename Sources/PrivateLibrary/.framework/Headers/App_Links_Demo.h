3a637e4eafc4521dedfcea624d5dca94abf324e2
prathameshb-cdi <prathamesh.b@connectingdotsinfotech.com>
2025-05-09 16:43:01 +0530

    first commit

---
+++ Sources/PrivateLibrary/.framework/Headers/App_Links_Demo.h
@@ -0,0 +1,18 @@
+//
+//  App_Links_Demo.h
+//  App-Links-Demo
+//
+//  Created by Prathamesh Bhise on 23/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for App_Links_Demo.
+FOUNDATION_EXPORT double App_Links_DemoVersionNumber;
+
+//! Project version string for App_Links_Demo.
+FOUNDATION_EXPORT const unsigned char App_Links_DemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <App_Links_Demo/PublicHeader.h>
+
+//#import <App_Links_Demo/CustomWebView>
