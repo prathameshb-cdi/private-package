c6778edc206c8777c08f36fa9b83ce58d54ffac9
prathameshb-cdi <prathamesh.b@connectingdotsinfotech.com>
2025-05-12 12:21:18 +0530

    -- Trying without importing the framework

--- Sources/PrivateLibrary/PrivateLibrary.swift
+++ Sources/PrivateLibrary/PrivateLibrary.swift
@@ -1,8 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import SimpleFramework
-
-public class HelloWorld: Hello {
-    
-}
+//import SimpleFramework
+//
+//public class HelloWorld: Hello {
+//    
+//}
